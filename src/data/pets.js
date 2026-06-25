cf2f59949507d34d64e11f9aa2d922f5bd0eba79
0952460525-lab <0952460525@aitec.edu.ec>
2026-06-25 17:30:58 -0500

    correcion de imagenes de perros y gatos

--- src/data/pets.js
+++ src/data/pets.js
@@ -14,7 +14,7 @@ export const perros = [
     raza: "Pastor Alemán",
     edad: "1 año",
     precio: 520,
-    imagen: "https://picsum.photos/id/201/300/200",
+    imagen:"https://images.unsplash.com/photo-1589941013453-ec89f33b5e95?q=80&w=1000&auto=format&fit=crop",
     descripcion: "Muy inteligente y protectora."
   },
   {
@@ -23,8 +23,35 @@ export const perros = [
     raza: "Bulldog Francés",
     edad: "3 años",
     precio: 380,
-    imagen: "https://picsum.photos/id/669/300/200",
-    descripcion: "Tranquilo y cariñoso."
+    imagen:"https://images.unsplash.com/photo-1583511655857-d19b40a7a54e?q=80&w=1069&auto=format&fit=crop",
+    descripcion: "Tranquilo y cariñoso.",
+    },
+    {
+    id: 4,
+    nombre: "Toby",
+    raza: "Beagle",
+    edad: "8 meses",
+    precio: 300,
+    imagen: "https://images.unsplash.com/photo-1543466835-00a7907e9de1?q=80&w=1074&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    descripcion: "Lleno de energía, perfecto para familias activas."
+  },
+  {
+    id: 5,
+    nombre: "Bella",
+    raza: "Labrador Retriever",
+    edad: "2 años",
+    precio: 480,
+    imagen: "https://images.unsplash.com/photo-1537204696486-967f1b7198c8?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    descripcion: "Cariñosa y excelente con niños, muy obediente."
+  },
+  {
+    id: 6,
+    nombre: "Trufa",
+    raza: "Husky Siberiano",
+    edad: "1 año",
+    precio: 550,
+    imagen: "https://images.unsplash.com/photo-1627220832671-b8fbf262f5cb?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    descripcion: "Aventurero y resistente, necesita mucho ejercicio diario."
   }
 ];
 
@@ -35,25 +62,26 @@ export const gatos = [
     raza: "Siamés",
     edad: "1.5 años",
     precio: 320,
-    imagen: "https://picsum.photos/id/40/300/200",
-    descripcion: "Elegante y muy vocal."
+    imagen:"https://images.pexels.com/photos/596590/pexels-photo-596590.jpeg?auto=compress&cs=tinysrgb&w=1000",    descripcion: "Elegante y muy vocal."
   },
   {
     id: 2,
     nombre: "Nala",
     raza: "Maine Coon",
-    edad: "2 años",
-    precio: 410,
-    imagen: "https://picsum.photos/id/1015/300/200",
-    descripcion: "Grande, peludo y sociable."
+    edad: "6 meses",
+    precio: 450,
+    imagen: "https://images.unsplash.com/photo-1533738363-b7f9aef128ce?q=80&w=1000&auto=format&fit=crop",
+    descripcion: "De gran tamaño, muy juguetona y cariñosa."
   },
   {
     id: 3,
     nombre: "Simba",
     raza: "Persa",
-    edad: "3 años",
-    precio: 290,
-    imagen: "https://picsum.photos/id/1074/300/200",
-    descripcion: "Tranquilo y de pelaje largo."
+    edad: "2 años",
+    precio: 400,
+    imagen:"https://images.pexels.com/photos/17767/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=1000",    
+    descripcion: "Tranquilo, le encanta tomar siestas al sol."
   }
 ];
+  
+
